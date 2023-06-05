48d9a6222c6baf6a79c0b014e8c2eeb3ba95a524
Odrexyo <92178848+Odrexyo@users.noreply.github.com>
2023-06-05 14:07:20 +0200

    Update Hub.lua

--- XeyHub/Hub.lua
+++ XeyHub/Hub.lua
@@ -1,12 +1,8 @@
-local StarterGui = game:GetService("StarterGui")
-
+local Nord = loadstring(game:HttpGetAsync("https://scripts.nord.vip/NordNotifs.lua", true))();
 local Link = game:HttpGet("https://raw.githubusercontent.com/Odrexyo/Script/main/XeyHub/Games/"..game.PlaceId..".lua")
 
 if Link then	
-  StarterGui:SetCore("SendNotification", {
-		Title = "XeyHub X Notification",
-		Text = "Welcome. Loading script.",
-		Duration = 10
-	})
+  _G.Config = { ["Theme"] = "Spotify" }
+Nord:Notify("XeyHub ", "Script Loaded Sucessfully ", "success", 7);
 	loadstring(Link)()
 end
