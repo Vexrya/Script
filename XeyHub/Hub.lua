3b7035b27ebe43bd1ed29eef402f1c532235c406
Xeyko <92178848+Odrexyo@users.noreply.github.com>
2023-08-02 12:48:42 +0200

    Update Hub.lua

--- XeyHub/Hub.lua
+++ XeyHub/Hub.lua
@@ -1,8 +1,8 @@
-local Nord = loadstring(game:HttpGetAsync("https://scripts.nord.vip/NordNotifs.lua", true))();
+--local Nord = loadstring(game:HttpGetAsync("https://scripts.nord.vip/NordNotifs.lua", true))();
 local Link = game:HttpGet("https://raw.githubusercontent.com/Odrexyo/Script/main/XeyHub/Games/"..game.PlaceId..".lua")
 
 if Link then	
-  _G.Config = { ["Theme"] = "Spotify" }
-Nord:Notify("XeyHub ", "Script Loaded Sucessfully ", "success", 7);
+  --_G.Config = { ["Theme"] = "Spotify" }
+--Nord:Notify("XeyHub ", "Script Loaded Sucessfully ", "success", 7);
 	loadstring(Link)()
 end
