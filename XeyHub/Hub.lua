b9c686dc21315551d5b943207924f0069a5fe351
Xeyko <92178848+Odrexyo@users.noreply.github.com>
2023-08-03 12:49:43 +0200

    Delete Hub.lua

--- XeyHub/Hub.lua
+++
@@ -1,8 +0,0 @@
---local Nord = loadstring(game:HttpGetAsync("https://scripts.nord.vip/NordNotifs.lua", true))();
-local Link = game:HttpGet("https://raw.githubusercontent.com/Odrexyo/Script/main/XeyHub/Games/"..game.PlaceId..".lua")
-
-if Link then	
-  --_G.Config = { ["Theme"] = "Spotify" }
---Nord:Notify("XeyHub ", "Script Loaded Sucessfully ", "success", 7);
-	loadstring(Link)()
-end
