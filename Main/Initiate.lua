3eafb454677526ccb9d035fb6a22f6fff51dd593
Vexrya <92178848+Vexrya@users.noreply.github.com>
2025-02-02 03:29:39 +0200

    Update and rename idk.lua to Initiate.lua

---
+++ Main/Initiate.lua
@@ -0,0 +1,31 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Initiate.lua"))()
+
+local getgenv: () -> ({[string]: any}) = getfenv().getgenv
+
+local function Notify(Text)
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "Notification",
+		Text = Text,
+		Duration = 10
+	})
+end
+
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Analytics.lua"))()
+
+local PlaceName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+PlaceName = PlaceName:gsub("%b[]", "")
+PlaceName = PlaceName:gsub("[^%a]", "")
+
+local Code: string = game:HttpGet(`https://raw.githubusercontent.com/Vexrya/Script/refs/heads/main/Games/{PlaceName}.lua`)
+
+if Code ~= "404: Not Found" then
+	Notify("Game found, the script is loading.")
+	getgenv().PlaceFileName = PlaceName
+else
+	Notify("Game not found, loading universal.")
+	getgenv().ScriptVersion = "Universal"
+	Code = game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Core.lua")
+end
+
+getgenv().FrostByteHandleFunction(loadstring(Code))
